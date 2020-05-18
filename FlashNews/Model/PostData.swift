84923f48ec6bc70606502d1e360f7f32de41b453
Abhas Kumar <abhasroy@gmail.com>
2020-05-17 21:18:22 -0700

    created skeleton

---
+++ FlashNews/Model/PostData.swift
@@ -0,0 +1,9 @@
+//
+//  PostData.swift
+//  FlashNews
+//
+//  Created by Abhas Kumar on 5/17/20.
+//  Copyright © 2020 Abhas Kumar. All rights reserved.
+//
+
+import Foundation
--- FlashNews/View/WebView.swift
+++ FlashNews/View/WebView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WebView.swift
 //  FlashNews
 //
 //  Created by Abhas Kumar on 5/17/20.
@@ -8,14 +8,14 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WebView: View {
     var body: some View {
-        Text("Hello, World!")
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct WebView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WebView()
     }
 }
